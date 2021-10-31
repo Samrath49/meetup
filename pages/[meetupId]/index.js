8633a4df7eee2ce8bde7579f4f3989ca4810f9af
Samrath Chauhan <samrathchauhan49@gmail.com>
2021-10-31 18:53:47 +0530

    Meetup Details done

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,16 +1,12 @@
-import { Fragment } from "react";
+import MeetupDetail from "../../components/meetups/MeetupDetail";
 
 function MeetupDetails() {
   return (
-    <Fragment>
-      <img
-        src="https://images.unsplash.com/photo-1572112533098-2785b64123da?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1332&q=80"
-        alt="Our first meetup"
+      <MeetupDetail image={"https://images.unsplash.com/photo-1572112533098-2785b64123da?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1332&q=80"} 
+      title={"Our first meetup"}
+      address={"London Eye"}
+      description={"The meetup description"}
       />
-      <h1>A First Meetup</h1>
-      <address>London Eye</address>
-      <p>The Meetup description</p>
-    </Fragment>
   );
 }
 
